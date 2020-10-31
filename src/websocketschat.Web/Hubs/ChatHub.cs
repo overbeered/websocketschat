3e1cbfc962e13dab085337845226b17d6435ff3c
Artur Safiullin <shard.f@mail.ru>
2020-10-31 19:48:55 +0300

    added new functionality to chat.

--- src/websocketschat.Web/Hubs/ChatHub.cs
+++ src/websocketschat.Web/Hubs/ChatHub.cs
@@ -20,5 +20,15 @@ namespace websocketschat.Web.Hubs
         {
             await Clients.All.SendAsync("Receive", message, userName);
         }
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.GetHttpContext().User.Identity.Name} вошел в чат");
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.GetHttpContext().User.Identity.Name} покинул в чат");
+            await base.OnDisconnectedAsync(exception);
+        }
     }
 }
