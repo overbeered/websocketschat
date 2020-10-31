cb00d20ec03d2aae0275e098e4b91e22dd3c7bf3
Artur Safiullin <shard.f@mail.ru>
2020-10-31 17:16:32 +0300

    added repository implementation and related mappers.

---
+++ src/websocketschat.Database.Repositories/BaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using websocketschat.Database.Context;
+
+namespace websocketschat.Database.Repositories
+{
+    /// <summary>
+    /// Shared part of every repository.
+    /// </summary>
+    public class BaseRepository : IDisposable
+    {
+        protected readonly NpgSqlContext _dbContext;
+        private bool disposedValue;
+
+        public BaseRepository(NpgSqlContext context)
+        {
+            _dbContext = context;
+        }
+
+        /// <summary>
+        /// Releases unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">Handle the process of releasing.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _dbContext.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        /// <summary>
+        /// Implements Dispose pattern.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
