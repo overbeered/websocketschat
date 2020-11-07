70f882fd8e17aa2bbe1ed74d2f7b9b443a4a9572
Artur Safiullin <shard.f@mail.ru>
2020-11-07 23:23:35 +0300

    implemented registration.

--- src/websocketschat.Web/Hubs/ChatHub.cs
+++ src/websocketschat.Web/Hubs/ChatHub.cs
@@ -4,7 +4,9 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using websocketschat.Core.Services.Interfaces;
 using websocketschat.Web.Helpers.MessageHandler;
 
 namespace websocketschat.Web.Hubs
@@ -13,25 +15,33 @@ namespace websocketschat.Web.Hubs
     public class ChatHub : Hub
     {
         private readonly ILogger<ChatHub> _logger;
-        public ChatHub(ILogger<ChatHub> logger)
+        private readonly MessageHandler _messageHandler;
+        public ChatHub(ILogger<ChatHub> logger, MessageHandler messageHandler)
         {
             _logger = logger;
+            _messageHandler = messageHandler;
         }
         public async Task Send(string message, string userName)
         {
-            string finalMessage = CommandHandler.Execute(message);
+            string senderUserRole = Context.User.FindFirst(ClaimTypes.Role)?.Value;
+
+            string finalMessage = await _messageHandler.HandleAsync(userName, message);
 
             await Clients.All.SendAsync("Receive", finalMessage, userName);
         }
         public override async Task OnConnectedAsync()
         {
-            Console.WriteLine("Подключился " + Context.GetHttpContext().User.Identity.Name);
+#if DEBUG
+            Console.WriteLine($"{Context.GetHttpContext().User.Identity.Name} вошел в чат");
+#endif
             await Clients.All.SendAsync("Notify", $"{Context.GetHttpContext().User.Identity.Name} вошел в чат");
             await base.OnConnectedAsync();
         }
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+#if DEBUG
             Console.WriteLine("Отключился " + Context.GetHttpContext().User.Identity.Name);
+#endif
             await Clients.All.SendAsync("Notify", $"{Context.GetHttpContext().User.Identity.Name} покинул в чат");
             await base.OnDisconnectedAsync(exception);
         }
