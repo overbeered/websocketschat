90fd11a9030b7d6b8a835386322d5c2dc54eb6c3
Artur Safiullin <shard.f@mail.ru>
2020-11-10 20:51:29 +0300

    added migration for isDeleted property and updated init file.

---
+++ src/websocketschat.Database.Context/Migrations/20201110174609_Added isDeleted property.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace websocketschat.Database.Context.Migrations
+{
+    public partial class AddedisDeletedproperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+        }
+    }
+}
