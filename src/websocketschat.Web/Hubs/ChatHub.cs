b6983f2ec5fb97f58897f6311a1c570c39faa0ee
Artur Safiullin <shard.f@mail.ru>
2020-11-24 17:11:41 +0300

    minor fixes.

--- src/websocketschat.Web/Hubs/ChatHub.cs
+++ src/websocketschat.Web/Hubs/ChatHub.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using websocketschat.Core.Models;
 using websocketschat.Core.Services.Interfaces;
